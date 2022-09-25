d573173eb9cd2595161227466ebf4a91992384ed
Muntakimur Rahaman <muntakim@innova-analytics.ai>
2022-09-25 16:18:42 +0600

    new version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import ThemeContext from "./ThemeContext";
+ReactDOM.render(
+  <ThemeContext>
+    <App />
+  </ThemeContext>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
